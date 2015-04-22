cba093c2035ca703400601e7402020045caddbb2
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-04-22 08:32:55 -0700

    milestone 2 added the index.php code

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+<title>Jacky's To-Do App 2</title>
+<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
+	<div class="wrap">
 
+	</div>
 </body>
-</html>
+</html
