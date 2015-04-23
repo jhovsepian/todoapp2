6ae7bd2ab7807580a6b06c2e75adf768bf4ccb5b
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-04-23 08:45:14 -0700

    milestone 8 added more code :)

--- includes/connect.php
+++ includes/connect.php
@@ -1,12 +1,12 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
-//we want it to die to then have this message
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+// we want it to die to then have this message have this message
 if ($mysqli->connect_error) {
 	die('Connect Error (' . $mysqli->connect_errno .')'
 		. $mysqli->connect_error);
 }
 else {
-	echo "Connection made";
+	// "Connection made";
 }
 $mysqli->close();
 
