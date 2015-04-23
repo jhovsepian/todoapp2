873745bc0f475232e2da23699d1fb0a3bedfd046
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-04-23 08:53:29 -0700

    milestone 9 getting info from the form

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Jacky's To-Do App 2</title>
+<title>Jacky's To-Do App</title>
 <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
@@ -16,4 +16,21 @@
 		</form>
 		</div>
 </body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+//calling the add task function
+	add_task();
+	function add_task() {
+		$('.add-new-task').submit(function() {
+			var new_task = $('.add-new-task input[name=new-task]').val();
+			if (new_task != '') {
+				$.post('includes/add-task.php', { task: new_task}, function(data) {
+					$(('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide().fadeIn();
+				});
+			}
+			return false;
+		});
+	}
+</script>
 </html>
